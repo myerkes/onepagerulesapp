46657a5c37c422de19437d1cba9f8bf60b389e52
Matthew Yerkes <mattyerkes23@gmail.com>
2021-10-12 22:49:53 -0400

    Add new models to admin

--- armyapp/admin.py
+++ armyapp/admin.py
@@ -1,3 +1,12 @@
+from armyapp.models import Unit
 from django.contrib import admin
+from .models import SpecialRule, Equipment, UpgradeListItems, Upgrade, PsychicSpell, Unit, Army
 
 # Register your models here.
+admin.site.register(SpecialRule)
+admin.site.register(Equipment)
+admin.site.register(UpgradeListItems)
+admin.site.register(Upgrade)
+admin.site.register(PsychicSpell)
+admin.site.register(Unit)
+admin.site.register(Army)
