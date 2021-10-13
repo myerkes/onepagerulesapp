effa3bdd537f54d20bd92d671cfc17fe2e2d1e81
Matthew Yerkes <mattyerkes23@gmail.com>
2021-10-13 19:04:38 -0400

    Restructure models and update admin views

--- armyapp/admin.py
+++ armyapp/admin.py
@@ -1,6 +1,6 @@
 from armyapp.models import Unit
 from django.contrib import admin
-from .models import SpecialRule, Equipment, UpgradeListItems, Upgrade, PsychicSpell, Unit, Army
+from .models import SpecialRule, Equipment, UpgradeListItems, Upgrade, PsychicSpell, Unit, Army, SpecialRuleUnitJoin
 
 # Register your models here.
 admin.site.register(SpecialRule)
@@ -10,3 +10,4 @@ admin.site.register(Upgrade)
 admin.site.register(PsychicSpell)
 admin.site.register(Unit)
 admin.site.register(Army)
+admin.site.register(SpecialRuleUnitJoin)
--- armyapp/models.py
+++ armyapp/models.py
@@ -4,41 +4,72 @@ from django.db import models
 class SpecialRule(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
-    value = models.IntegerField
+
+    def __str__(self) -> str:
+        return self.name
 
 class Equipment(models.Model):
     name = models.CharField(max_length=255)
-    range = models.IntegerField
-    attacks = models.IntegerField
-    specialrules = models.ManyToManyField(SpecialRule)
+    range = models.IntegerField(default=0)
+    attacks = models.IntegerField(default=0)
+    specialrules = models.ManyToManyField(SpecialRule, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class UpgradeListItems(models.Model):
     name = models.CharField(max_length=255)
-    cost = models.IntegerField
+    cost = models.IntegerField(default=0)
 
-class Upgrade(models.Model):
-    name = models.CharField(max_length=255)
-    upgradelistitems = models.ManyToManyField(UpgradeListItems)
+    def __str__(self) -> str:
+        return self.name
+
 
 class PsychicSpell(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
-    quality = models.IntegerField
+    quality = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Unit(models.Model):
     name = models.CharField(max_length=255)
-    size = models.IntegerField
-    quality = models.IntegerField
-    defense = models.IntegerField
-    cost = models.IntegerField
-    equipments = models.ManyToManyField(Equipment)
-    specialrules = models.ManyToManyField(SpecialRule)
-    upgrades = models.ManyToManyField(Upgrade)
+    size = models.IntegerField(default=0)
+    quality = models.IntegerField(default=0)
+    defense = models.IntegerField(default=0)
+    cost = models.IntegerField(default=0)
+    equipments = models.ManyToManyField(Equipment, blank=True)
+    upgrades = models.ManyToManyField('Upgrade', blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class SpecialRuleUnitJoin(models.Model):
+    value = models.IntegerField(blank=True, null=True)
+    specialrule = models.ForeignKey(SpecialRule, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        if self.value:
+            return f'{self.specialrule.name}({self.value}) - {self.unit.name }'
+        else:
+            return f'{self.specialrule.name} - {self.unit.name }'
 
 class Army(models.Model):
     name = models.CharField(max_length=255)
     background = models.CharField(max_length=1000)
-    units = models.ManyToManyField(Unit)
-    specialrules = models.ManyToManyField(SpecialRule)
-    upgrades = models.ManyToManyField(Upgrade)
-    pyschicspells = models.ManyToManyField(PsychicSpell)
+    units = models.ManyToManyField(Unit, blank=True)
+    specialrules = models.ManyToManyField(SpecialRule, blank=True)
+    pyschicspells = models.ManyToManyField(PsychicSpell, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Upgrade(models.Model):
+    name = models.CharField(max_length=255)
+    upgradelistitems = models.ManyToManyField(UpgradeListItems, blank=True)
+    army = models.ForeignKey(Army, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
